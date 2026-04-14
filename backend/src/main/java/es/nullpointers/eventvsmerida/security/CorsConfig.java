69953a22fa35f7ee06132e48bbdf26154bbd40ba
aperezm1 <aperezm111@iesalbarregas.es>
2026-04-14 16:37:17 +0200

    Añadida config de CORS

---
+++ backend/src/main/java/es/nullpointers/eventvsmerida/security/CorsConfig.java
@@ -0,0 +1,34 @@
+package es.nullpointers.eventvsmerida.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    private static final String[] ALLOWED_ORIGINS = {
+            "https://eventvsmerida.onrender.com",
+            "http://localhost:5501",
+            //"https://eventvsmerida-admin.vercel.app"
+    };
+    private static final String[] ALLOWED_METHODS = { "GET", "POST", "PUT", "DELETE", "OPTIONS" };
+    private static final String[] ALLOWED_HEADERS = { "Content-Type", "Authorization", "X-Requested-With" };
+    private static final String[] EXPOSED_HEADERS = { "Authorization" };
+    private static final long MAX_AGE = 3600;
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(ALLOWED_ORIGINS)
+                        .allowedMethods(ALLOWED_METHODS)
+                        .allowedHeaders(ALLOWED_HEADERS)
+                        .exposedHeaders(EXPOSED_HEADERS)
+                        .maxAge(MAX_AGE);
+            }
+        };
+    }
+}
