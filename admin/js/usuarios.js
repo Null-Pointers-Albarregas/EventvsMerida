d4c54c093a62a8fd5621e00e7c25a679de3b0d03
David Muñoz <dmunozc04@iesalbarregas.es>
2026-04-27 09:03:32 +0200

    Cambio del endpoint de login

--- admin/js/usuarios.js
+++ admin/js/usuarios.js
@@ -1,8 +1,57 @@
 window.addEventListener("DOMContentLoaded", async (event) => {
-    const URL_BASE = "https://eventvsmerida.onrender.com/api/";
+  const URL_BASE = "https://eventvsmerida.onrender.com/api/";
 
-  const select = document.getElementById("roles");
+  obtenerRoles(URL_BASE);
+});
 
+async function obtenerUsuarios(URL_BASE) {
+  try {
+    const resp = await fetch(URL_BASE + "usuarios/all", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include"
+    });
+
+    if (!resp.ok) {
+      throw new Error("Error al obtener los usuarios");
+    }
+
+    const tabla = document.getElementById("listadoUsuarios");
+
+    const data = await resp.json();
+    data.forEach((rol) => {
+      const opt = document.createElement("option");
+      
+      select.appendChild(opt);
+
+      // Limpia validaciones y campos al cerrar el modal de usuario
+      const modalUsuario = document.getElementById("modalCrearUsuario");
+      if (modalUsuario) {
+        modalUsuario.addEventListener("hidden.bs.modal", function () {
+          const form = document.getElementById("formAgregarUsuario");
+          if (form) {
+            form.classList.remove("was-validated");
+            form.reset();
+          }
+        });
+      }
+
+      modalUsuario.addEventListener("submit", async function (event) {
+        event.preventDefault();
+        const form = document.getElementById("formAgregarUsuario");
+        if (form.checkValidity()) {
+          mostrarAlerta("success", "Usuario creado correctamente");
+        }
+      });
+    });
+  } catch (error) {
+    console.error("Error al cargar los roles:", error);
+  }
+}
+
+async function obtenerRoles(URL_BASE) {
   try {
     const resp = await fetch(URL_BASE + "roles/all", {
       method: "GET",
@@ -14,6 +63,8 @@ window.addEventListener("DOMContentLoaded", async (event) => {
     if (!resp.ok) {
       throw new Error("Error al obtener los roles");
     }
+
+    const select = document.getElementById("roles");
 
     const data = await resp.json();
     data.forEach((rol) => {
@@ -57,4 +108,4 @@ window.addEventListener("DOMContentLoaded", async (event) => {
   } catch (error) {
     console.error("Error al cargar los roles:", error);
   }
-});
+}
