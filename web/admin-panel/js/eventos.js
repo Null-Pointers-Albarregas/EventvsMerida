552464c1820c60338472bae7aa71c336b328abb0
eventvsmerida <eventvsmerida@gmail.com>
2026-05-05 17:15:40 +0200

    Update eventos.js

--- web/admin-panel/js/eventos.js
+++ web/admin-panel/js/eventos.js
@@ -1,4 +1,4 @@
-const URL_BASE = "https://eventvsmerida.onrender.com/api/";
+const URL_BASE = "https://eventvsmerida-x2t1.onrender.com/api/";
 
 window.addEventListener("DOMContentLoaded", async () => {
   const sesion = await logeado();
